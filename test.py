2ffc50724f54532f3931e25e0cf25b6986ba7ca8
Cloud <cloud@noreply.localhost>
2022-12-10 22:29:16 -0800

    Update 'test.py'

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
+#importing file
 from ets2sdkclient import ets2sdkclient
+#Stuff For Speed Limit And Cruise
+import math
+#Giving it cool names
 e = ets2sdkclient()
+#Updating the values
 e.update()
-e.printall()
+#v < Use this to print all 
+#e.printalll()
+
+#Setting the values
+limit = int(e.speedLimit * 3.6)
+speed = math.ceil(e.cruiseControlSpeed * 3.6)
+#Printing
+print(limit)
+print(speed)
